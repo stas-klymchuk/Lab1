eaedf1f663de5b93c67a367004be8e86386c600c
Stas klymchuk <klimchuk2010stas@gmail.com>
2026-09-08 23:03:54 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+
+# Змінні
+a = 15
+b = 4.5
+name = "Stas"
+is_ok = True
+
+print("a =", a, type(a))
+print("b =", b, type(b))
+print("name =", name, type(name))
+print("is_ok =", is_ok, type(is_ok))
+
+# Оператори
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+print(a > 10)
+print(b == 4.5)
+print(name == "Stas")
+
+print(is_ok and True)
+print(False or True)
+print(not is_ok)
